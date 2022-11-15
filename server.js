32e923302eecf75288d533cf77b877de32702ab5
imjohnnyy <ldong4056@gmail.com>
2022-11-15 15:20:27 +1300

    Setup Node and Express

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+// Starts the server in Port ____, if it is successful then something is returned 
+// (A message in this context)
+app.listen(port, () => console.log(`App is listening on port ${port}`));
