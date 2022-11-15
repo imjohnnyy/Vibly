32a10daf8f2b521e04373d349cfe9feb83e819fb
imjohnnyy <ldong4056@gmail.com>
2022-11-15 18:20:06 +1300

    Added register form and styling

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,41 @@
+import React, { useState } from "react";
+
 function Register() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const register = () => {
+    console.log(user);
+  }
   return (
-    <div>Register</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-3 w-96 p-6 shadow border border-gray-400">
+        <h1 className="text-3xl font-bold text-gray-700">Welcome to Vibly</h1>
+        <hr />
+        <input
+          type="text"
+          placeholder="Name"
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button className="primary" onClick={register}>Register</button>
+      </div>
+    </div>
+  );
 }
-export default Register
+export default Register;
